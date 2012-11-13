7cf6a3914b74dbf049b09acc2742feb0aa492931
JamesFW <james.wu@students.olin.edu>
2012-11-12 23:36:13 -0500

    fleshed out the server architecture in the comments put in potential libraries to use for the diff change protocol

--- seamless.py
+++ seamless.py
@@ -1,8 +1,10 @@
 import sublime, sublime_plugin
 
-# create an asynchronous communication stream (messaging protocol)
+# create an asynchronous communication stream (messaging protocol) http://coder.cl/products/pyxser/
+# http://xmpppy.sourceforge.net/ 
 # have to send the 'view' object on modified to clients
-# use the picklejson library to serialize the 'view' object on both ends
+# use the pyxser library to serialize the 'view' object on both ends
+# http://coder.cl/products/pyxser/
 
 
 class ExampleCommand(sublime_plugin.TextCommand):
@@ -12,10 +14,45 @@ class ExampleCommand(sublime_plugin.TextCommand):
 		TO DO :
 
 		sublime EVENT LISTEn on_modified(view)	
-			seialize view into json
+			seialize view into xml
 			send to client
 			
 		
 		seamless event listener on_receive(data)
-			unserialize(data) from json to view
+			unserialize(data) from xml to view
 			update current view with data
+
+            two global variables: sent_data and received_data
+            server writes to received_data and reads send_data
+            sublime writes to send_data and reads recevied_dataS
+           """
+class Server():
+    def run(self, text):
+        """
+        requirements:
+        (i'm reading from a constantly updated global variables)
+        user A: i will have a listener which listens for changes in a global variable (send_data)
+        upon an event, the listener will create an XML stream which is sent over a protocol to the IP
+        server: will have a listener which listens for XML stream activity (from user B)
+        when it receives an XML event, it will update user A's read_data with the information in the tag
+
+
+
+
+        possible methods of sending data
+        1. xmpp
+
+        2. amqp
+        http://www.rabbitmq.com/devtools.html#python-dev
+        http://barryp.org/software/py-amqplib/
+        http://ask.github.com/carrot/introduction.html
+
+        later
+        finding clients not hardwired through IP
+
+
+        later later
+        xmpp over github
+
+
+        """ 
