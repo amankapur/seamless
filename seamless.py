3b692c7758f3ed0239308cb0803c0803eba71483
Aman Kapur <amankapur91@gmail.com>
2012-11-12 22:51:47 -0500

    todos added

--- seamless.py
+++ seamless.py
@@ -1,5 +1,21 @@
 import sublime, sublime_plugin
 
+# create an asynchronous communication stream (messaging protocol)
+# have to send the 'view' object on modified to clients
+# use the picklejson library to serialize the 'view' object on both ends
+
+
 class ExampleCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		self.view.insert(edit, 0, "Hello, World!")
+		""" 
+
+		TO DO :
+
+		sublime EVENT LISTEn on_modified(view)	
+			seialize view into json
+			send to client
+			
+		
+		seamless event listener on_receive(data)
+			unserialize(data) from json to view
+			update current view with data
