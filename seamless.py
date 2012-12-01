041e4271e8a4b36730d790f4571782bb751bc349
Aman Kapur <amankapur91@gmail.com>
2012-12-01 16:09:01 -0500

    threading for recv_data not working

--- seamless.py
+++ seamless.py
@@ -2,7 +2,8 @@ import sublime, sublime_plugin
 import pickle as xml_pickle
 import json
 import inspect
-import threading
+import thread
+from time import sleep
 
 # create an asynchronous communication stream (messaging protocol) http://coder.cl/products/pyxser/
 # http://xmpppy.sourceforge.net/ 
@@ -10,69 +11,37 @@ import threading
 # use the pyxser library to serialize the 'view' object on both ends
 # http://coder.cl/products/pyxser/
 
-Send_data = 0
-Recv_data = 0
 
 class SeamlessCommand(sublime_plugin.EventListener):
-                   
+        
+        Send_data = 0
+        Recv_data = 0
+
+        def on_load(self, view):                
+                print 'thread runnin'
+                print self.Recv_data, 'printing'
+                #thread.start_new_thread(self.update, (view,''))
+
         def on_modified(self, view):
-                reg = sublime.Region(0,10000000)
-                Send_data = str(view.substr(reg))
-                print Send_data
-                print view.sel()
-                try:
-                        thread.start_new_thread(self.update(view))
-                except:
-                        print "Error: unable to start thread"
 
+                reg = sublime.Region(0, 10000)
+                self.Send_data = str(view.substr(reg))
+                print self.Send_data
+                #print view.sel()
+                print "onmod recv", self.Recv_data
+                self.update(view, '')
+                        
 
+        def on_post_save(self, view):
+                print "on_save called"
+                self.Recv_data = "apples"
+                print self.Recv_data
 
-        def update(self, view):
-                while 1:
-                        if(recv_data != 0):
+        def update(self, view, string):
+                print "recv", self.Recv_data
+                while 1: 
+                        if(self.Recv_data != 0):
                                 edit = view.begin_edit()
-                                view.insert(edit, 0, recv_data)
+                                view.insert(edit, 0, self.Recv_data)
+                                self.Recv_data = 0
 
-        """
-        TO DO :
-                sublime EVENT LISTEn on_modified(view)	
-                seialize view into xml
-                send to client
-
-
-        seamless event listener on_receive(data)
-        unserialize(data) from xml to view
-        update current view with data
-
-    two global variables: sent_data and received_data
-    server writes to received_data and reads send_data
-    sublime writes to send_data and reads recevied_dataS
-    """
-    class Server():
-            def run(self, text):
-                    """
-                    requirements:
-                            (i'm reading from a constantly updated global variables)
-                            user A: i will have a listener which listens for changes in a global variable (send_data)
-                            upon an event, the listener will create an XML stream which is sent over a protocol to the IP
-                            server: will have a listener which listens for XML stream activity (from user B)
-                            when it receives an XML event, it will update user A's read_data with the information in the tag
-
-
-        possible methods of sending data
-        1. xmpp
-
-        2. amqp
-        http://www.rabbitmq.com/devtools.html#python-dev
-        http://barryp.org/software/py-amqplib/
-        http://ask.github.com/carrot/introduction.html
-
-        later
-        finding clients not hardwired through IP
-
-
-        later later
-        xmpp over github
-
-
-        """ 
