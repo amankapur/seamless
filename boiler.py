0115d6fab11cdcc35b547c025d36559a0334ba7f
JamesFW <james.wu@students.olin.edu>
2012-12-02 19:45:06 -0500

    XMPP client for sending and receiving

--- boiler.py
+++ boiler.py
@@ -10,9 +10,10 @@ class SeamlessChat(ClientXMPP):
         ClientXMPP.__init__(self, jid, password)
         self.Recv_data = 0
         self.Send_data = 0
+        
 
-        #self.add_event_handler("session_start", self.session_start)
-        #self.add_event_handler("message", self.Recv_data)
+        self.add_event_handler("session_start", self.session_start)
+        self.add_event_handler("message", self.recvMessage)
 
     def session_start(self, event):
         self.send_presence()
@@ -20,12 +21,12 @@ class SeamlessChat(ClientXMPP):
 
     def recvMessage(self, msg):
         self.Recv_data = msg;
+        print "******************************************" + self.Recv_data
 
     def sendMessage(self, msg):
-        self.send_message(mto="butteryseamless@gmail.com",
-                          mbody=self.msg,
+        self.send_message(mto="james.wu.jimmy@gmail.com",
+                          mbody=msg,
                           mtype='chat')
-
 
 
 if __name__ == '__main__':
@@ -33,8 +34,13 @@ if __name__ == '__main__':
                         format='%(levelname)-8s %(message)s')
 
     xmpp = SeamlessChat('butteryseamless@gmail.com', 'OlinCollege')
+    xmpp.use_signals(signals=['SIGHUP','SIGTERM','SIGINT'])
     xmpp.connect()
-    xmpp.process(block=True)
+    xmpp.process(block=False)
 
-    inpu = raw_input("Enter message: ")
-    self.sendMessage(inpu)
+    while 1:
+     inpu = raw_input("Enter message: ")
+     if inpu == "fuck":
+         xmpp.disconnect()
+     else:
+         xmpp.sendMessage(inpu)
