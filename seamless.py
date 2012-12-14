56e838aaac12ab9a4f8c5663589db6b23e216335
Aman Kapur <amankapur91@gmail.com>
2012-12-13 23:59:12 -0500

    test v1

--- seamless.py
+++ seamless.py
@@ -10,33 +10,37 @@ class SeamlessCommand(sublime_plugin.EventListener):
         Recv_data = 0
         send_path = "/tmp/seamless-send"
         recv_path = "/tmp/seamless-recv"
-        server = ''
         client = ''
+        server = ''
 
-        def on_load(self, view):                
+
+        def on_load(self, view):              
                 print 'on_load'
-                #self.client = self.start_sendIPC()
+                self.client = self.start_sendIPC()
                 #t = Thread(target = self.start_recvIPC(), args=(self, ''))
-                self.start_recvIPC()
+                self.server = self.start_recvIPC()
+                print self.server
+                self.server.settimeout(0.0)
 
         def on_modified(self, view):
 
                 reg = sublime.Region(0, 10000)
                 self.Send_data = str(view.substr(reg))
                 print self.Send_data
-                #self.client.send(self.Send_data)
+                self.client.send(self.Send_data)
                 self.update(view, '')
 
+                
         def on_post_save(self, view):
                 #print "on_save called"
                 self.Recv_data = "apples"
-                print self.Recv_data
+                #print self.Recv_data
 
 
         def update(self, view, string):
                 print "recv", self.Recv_data
-                datagram = self.server.recv( 1024 )
-                        
+               #print datagram
+                datagram = self.server.recv(1024)
                 if datagram :
                         edit = view.begin_edit()
                         view.insert(edit, 0, datagram)
@@ -47,7 +51,7 @@ class SeamlessCommand(sublime_plugin.EventListener):
                 if os.path.exists(self.send_path):
                         client = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
                         client.connect(self.send_path)
-                        print "connected"
+                        print "send connected"
                         return client
                 else:
                         print "path doesnt exist, creating"
@@ -55,6 +59,9 @@ class SeamlessCommand(sublime_plugin.EventListener):
         def start_recvIPC(self):
                 if os.path.exists(self.recv_path):
                         os.remove(self.recv_path)
-                self.server = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
-                self.server.bind(self.recv_path)
+                server = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+                server.bind(self.recv_path)
+                print "recv connected"
+                return server
+
                                        
